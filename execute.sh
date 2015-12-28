c1a0c61b64cb459a7cca44c078a68807857da77c
tianyl1984 <tianyl1984@qq.com>
2015-12-28 15:31:02 +0800

    添加执行脚本

---
+++ execute.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ ! -n "$1" ]; then
+  echo "未指定执行的类"
+  exit
+fi
+cd /home/pi/pidemo
+git pull;
+mvn -f /home/pi/pidemo/pom.xml clean package;
+
+sudo java -classpath /opt/pi4j/lib/'*':/home/pi/pidemo/target/webapp/WEB-INF/classes com.tianyl.pidemo.$1
+
