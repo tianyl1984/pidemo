f014dfb96c09bd63b3dd016d6471eeb69dadeae0
tianyl1984 <tianyl1984@qq.com>
2015-12-28 15:18:39 +0800

    修改测试

--- src/main/java/com/tianyl/pidemo/Test.java
+++ src/main/java/com/tianyl/pidemo/Test.java
@@ -3,6 +3,6 @@ package com.tianyl.pidemo;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("a simple test!!");
+		System.out.println("a simple test!!one");
 	}
 }
